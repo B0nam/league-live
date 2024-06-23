5d99bb96df03be166bc024745669a9371757a700
KauanRossi <kauan.rossi@ufly.com.br>
2024-06-23 10:25:36 -0300

    Adicionado decorator para visualização das propriedades dos dtos no swagger

--- src/auth/dtos/create-user.dto.ts
+++ src/auth/dtos/create-user.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, MinLength } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty()
   @IsNotEmpty()
   @MinLength(8)
   username: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @MinLength(8)
   password: string;
