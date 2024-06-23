1db25921e4326503e56be5b486953aa18a34e680
Daniel Bonam <daniiel.bonam@gmail.com>
2024-06-23 03:20:18 -0300

    merge fix
    merge fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,37 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LeagueDataModule } from './league-data/league-data.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { LoggerModule } from './logger/logger.module';
+import { RequestLogMiddleware } from './logger/middlewares/request-log.middleware';
 import { LeagueStatsModule } from './league-stats/league-stats.module';
 
 @Module({
-  imports: [LeagueDataModule, LeagueStatsModule],
+  imports: [
+    LeagueDataModule,
+    LeagueStatsModule,
+    AuthModule,
+    LoggerModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(RequestLogMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
