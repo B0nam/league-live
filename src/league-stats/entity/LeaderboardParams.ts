7a6406de200d521b981a3857927bc8c669e7f5e5
Daniel Bonam <daniiel.bonam@gmail.com>
2024-06-23 22:03:05 -0300

    Update Class-Validator

--- src/league-stats/entity/LeaderboardParams.ts
+++ src/league-stats/entity/LeaderboardParams.ts
@@ -1,16 +1,19 @@
-import { IsString } from "class-validator";
+import { IsEnum, IsString } from "class-validator";
 import { QueueType } from "./queue-type.enum";
 import { TierType } from "./tier-type.enum";
 import { DivisionType } from "./division-type.enum";
 
 export class LeaderboardParams {
     @IsString()
+    @IsEnum(QueueType)
     queue: QueueType;
   
     @IsString()
+    @IsEnum(TierType)
     tier: TierType;
   
     @IsString()
+    @IsEnum(DivisionType)
     division: DivisionType;
   }
   
